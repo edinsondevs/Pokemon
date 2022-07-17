5c2f5703f2fc3c3c03be1292e7ac93804decb61b
Edinson <edinsonmadrid@gmail.com>
2022-07-17 19:49:29 -0300

    Update Constants

--- api/src/utils/GlobalConst.js
+++ api/src/utils/GlobalConst.js
@@ -1,9 +1,9 @@
-const URL_API_POKEMON = "https://pokeapi.co/api/v2/pokemon?limit=40"   //url con los primeros 40 pokemons
-const URL_API_POKEMON_TYPE = "https://pokeapi.co/api/v2/type";
-const URL_API_POKEMON_NAME_OR_ID = "https://pokeapi.co/api/v2/pokemon/";
+const URL_POKEMON = "https://pokeapi.co/api/v2/pokemon?limit=40"                //URL PARA TRAER SOLO 40 POKEMONES
+const URL_POKEMON_TYPE = "https://pokeapi.co/api/v2/type";                      //URL PARA TRAER LOS TIPOS DE POKEMONES
+const URL_POKEMON_NAME_OR_ID = "https://pokeapi.co/api/v2/pokemon/";            //URL PARA TRAER POKEMONES POR NAME O ID
 
 module.exports = {
-    URL_API_POKEMON, 
-    URL_API_POKEMON_TYPE,
-    URL_API_POKEMON_NAME_OR_ID
+    URL_POKEMON, 
+    URL_POKEMON_TYPE,
+    URL_POKEMON_NAME_OR_ID
 }
