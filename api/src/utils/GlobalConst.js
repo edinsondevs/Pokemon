1a27433ab3ee9d1f447d7de814df23a8a5b180ec
Edinson <edinsonmadrid@gmail.com>
2022-07-22 10:12:14 -0300

    Update Link acceso

--- api/src/utils/GlobalConst.js
+++ api/src/utils/GlobalConst.js
@@ -1,4 +1,4 @@
-const URL_POKEMON = "https://pokeapi.co/api/v2/pokemon?limit=40"                //URL PARA TRAER SOLO 40 POKEMONES
+const URL_POKEMON = "https://pokeapi.co/api/v2/pokemon"                //URL PARA TRAER SOLO 40 POKEMONES
 const URL_POKEMON_TYPE = "https://pokeapi.co/api/v2/type";                      //URL PARA TRAER LOS TIPOS DE POKEMONES
 const URL_POKEMON_NAME_OR_ID = "https://pokeapi.co/api/v2/pokemon/";            //URL PARA TRAER POKEMONES POR NAME O ID
 
