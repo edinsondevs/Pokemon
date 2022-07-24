643ee295d7cf97c25b2f3de76c35b7f8a14eb5a9
Edinson <edinsonmadrid@gmail.com>
2022-07-24 19:23:55 -0300

    update reducers

--- client/src/Reducers/Reducers.js
+++ client/src/Reducers/Reducers.js
@@ -1,15 +1,19 @@
 const initialState = {
     typePoke: [],
     allPokemons: [],
-    orderPokemons: [],
-    searchByType: [],
-    searchByCreation: [],
-    searchByName: [],
+    clonPokemons: [],
 
 }
 export default function reducer(state = initialState, action) {
 
     switch (action.type) {
+        //  *********************************           BUSQUEDA DE TODOS LOS POKEMONS     ****************************************************************************/
+        case 'GET_ALL_POKE':
+            return {
+                ...state,
+                allPokemons: action.payload,
+                clonPokemons: action.payload
+            }
         //  *********************************           BUSQUEDA DE TODOS LOS TIPOS DE POKEMONS     ***********************************************************************/
         case 'GET_TYPES_POKEMON':
             return {
@@ -17,41 +21,37 @@ export default function reducer(state = initialState, action) {
                 typePoke: action.payload
             }
 
-        //  *********************************           BUSQUEDA DE TODOS LOS POKEMONS     ****************************************************************************/
-        case 'GET_ALL_POKE':
-            return {
-                ...state,
-                allPokemons: action.payload
-            }
         //  ********************************************           FILTRO POR TYPO        ********************************************************************************/
 
         case 'SEARCH_BY_TYPE':
-            let searchByType = state.allPokemons
-            let pokemonsByType = searchByType.filter(e => e.type.includes(action.payload))
+            let searchByType = state.clonPokemons
+            let pokemonsByType = action.payload === 'All' ? searchByType 
+                : searchByType.filter(e => e.type.includes(action.payload))
+           // let pokemonsByType = pokemonsType.length !== 0 ? pokemonsType : pokemonsType.push("NotFound")
+            if (pokemonsByType.length === 0){
+                // pokemonsByType.push("NotFound")
+                pokemonsByType.push("NotFound")
+            }
             return {
                 ...state,
                 allPokemons: pokemonsByType
-                // searchByType: pokemonsByType
             }
         //  ********************************************           FILTRO POR CREACION    *******************************************************************************/        
 
         case 'SEARCH_BY_CREATION':
-            // let creating = action.payload
-            // console.log(creating)
-            let searchCreation = state.allPokemons
+            let searchCreation = state.clonPokemons
             let respCreation = action.payload === 'pokeDb' ?
                 searchCreation.filter(e => e.createdInDb) :
                 searchCreation.filter(e => !e.createdInDb)
             return {
                 ...state,
                 allPokemons: respCreation
-                // searchByCreation: respCreation
             }
 
         //  ********************************************           ORDENAMINETOS          ********************************************************************************/
         case 'GET_ORDERING':                                //                    ORDENAMINETO POR NOMBRE DE MAYOR A MENOR
             let orderby = action.payload
-            let orderPokemons = state.allPokemons
+            let orderPokemons = state.clonPokemons
             if (orderby === 'desc') {
                 orderPokemons.sort((a, b) => {
                     const nameA = a.name.toLocaleLowerCase()
@@ -81,17 +81,16 @@ export default function reducer(state = initialState, action) {
                     return 0
                 })
             }
+            console.log(orderPokemons)
             return {
                 ...state,
                 allPokemons: orderPokemons
-                // orderPokemons: orderPokemons
             }
         //  ********************************************           BUSQUEDA POR NOMBRE     ********************************************************************************/
         case 'SEARCH_BY_NAME':
             return ({
                 ...state,
                 allPokemons: action.payload
-                // searchByName: action.payload
             })
 
         default:
