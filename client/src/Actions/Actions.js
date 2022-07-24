66a7c01a1f6f033220cbdde7d80ff9531732997e
Edinson <edinsonmadrid@gmail.com>
2022-07-24 04:30:20 -0300

    update actions

--- client/src/Actions/Actions.js
+++ client/src/Actions/Actions.js
@@ -11,12 +11,57 @@ export const typePokemons = () => {
     
 }
 
-export const allPokemons = () => {
+export const getAllPokemons = () => {
     return async (dispatch) => {
-        let allPokemons = await axios.get('http://localhost:3001/pokemons');
+        let getAllPokemons = await axios.get('http://localhost:3001/pokemons');
         return dispatch({
             type: 'GET_ALL_POKE',
-            payload: allPokemons.data
+            payload: getAllPokemons.data
+        })
+    }
+}
+
+export const searchByType = (payload) => {
+    console.log(payload)
+    return {
+        type: 'SEARCH_BY_TYPE',
+        payload,
+    }
+}
+
+export const searchByCreation = (payload) => {
+    return { 
+        type: 'SEARCH_BY_CREATION',
+        payload
+    }
+}
+
+export const getOrdering = (payload) => {
+    return {
+        type: 'GET_ORDERING',
+        payload
+    }
+}
+
+export const searchByName = (payload) => {
+    return async function (dispatch) {
+        let json = await axios.get('https://pokeapi.co/api/v2/pokemon/'+payload);
+        let dataApi = json.data
+        const getPoke = {
+            id: dataApi.id,
+            sprite: dataApi.sprites.other.home.front_default,
+            name: dataApi.name,
+            hp: dataApi.stats[0].base_stat,
+            attack: dataApi.stats[1].base_stat,
+            defense: dataApi.stats[2].base_stat,
+            speed: dataApi.stats[5].base_stat,
+            height: dataApi.height,
+            weight: dataApi.weight,
+            type: dataApi.types.map(e => e.type.name)
+        }
+        return dispatch({
+            type: 'SEARCH_BY_NAME',
+            payload: getPoke
         })
     }
 }
