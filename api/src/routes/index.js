d3c813a44d4a9ca644fc13899ee1828b42484150
Edinson <edinsonmadrid@gmail.com>
2022-08-03 16:25:00 -0300

    add delete in routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -9,13 +9,15 @@ const app = Router();
 const typeRoutes = require('../controller/typesPokemon.js');
 const allPokemons = require('../controller/allPokemon.js');
 const addPokemons = require('../controller/addPokemon.js');
+const deletePokemons = require('../controller/deletePokemon.js');
 
 //*************************************************************************************************************************************************************/
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 app.use('/types', typeRoutes)
 app.use('/pokemons', allPokemons)
-app.use('/pokemons', addPokemons);
+app.use('/pokemons', addPokemons)
+app.use('/delete', deletePokemons);
 
 
 //*************************************************************************************************************************************************************/
