3d6ddb92552f42f7fd30369234873299ed5008fb
Edinson <edinsonmadrid@gmail.com>
2022-08-13 18:14:47 -0300

    add pageError pages

---
+++ client/src/Components/Pages/PageError.js
@@ -0,0 +1,19 @@
+import '../Css/Page404.css'
+
+
+
+function PageError() {
+    return (
+        <>
+            <div className="error">404</div>
+            <br /><br />
+            <span className="info">Page not found</span>
+                <div className="action-link-wrap">
+                    <a href="/home" className="link-button">Go to Home Page</a>
+                </div>
+        </>
+
+    )
+}
+
+export default PageError;
