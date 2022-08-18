fdc51610e3a0935f965ab6025d93d01bc59f2835
Edinson <edinsonmadrid@gmail.com>
2022-08-18 02:18:15 -0300

    update Home pages

--- client/src/Components/Pages/Home.js
+++ client/src/Components/Pages/Home.js
@@ -21,7 +21,7 @@ function Home() {
     const [pokemonsPerPage, setPokemonsPerPage] = useState(12)      //  cantidad de pokemones por pagina
     const indexLastPage = currentPage * pokemonsPerPage             //  Index del ultimo pokemon, multiplico la pag actual por la cantida de poke por paginas
     const [refresh, setRefresh] = useState(false)                   //  Estado para refrescar la pagina
-    
+
 
     //!------------------------------------------    DECLARACION DE LOS CONSTANTES  ------------------------------------------
     const indexOfFirtPokemon = indexLastPage - pokemonsPerPage;     // indice del ultimo poke - los poke por pagina
@@ -36,30 +36,36 @@ function Home() {
         dispatch(typePokemons())
         dispatch(resetPokemonDetail([]))
     }, [dispatch])
-    
+
     //!------------------------------------------    DECLARACION DE LAS FUNCIONES   ------------------------------------------
     function handleClick(e) {
         e.preventDefault()
         dispatch(getAllPokemons())
     }
-    
+
     const ordering = (e) => {
         setRefresh(!refresh)
         dispatch(getOrdering(e))
     }
-    
+
     const searchType = (e) => {
         dispatch(searchByType(e))
     }
-    
+
     const searcrCreation = (e) => {
         dispatch(searchByCreation(e))
     }
-    
+
     const handleInputName = (e) => {
         setName(e)
     }
-    
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            handleSubmit(e)
+        }
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
         if (name === '') {
@@ -68,17 +74,25 @@ function Home() {
         dispatch(searchByName(name))
         setName('')
     }
-    
+
     //------------------------------------------    COMPONENTE A RENDERIZAR     ------------------------------------------
     return (
         <div className='cmp-container-home'>
             {/* //! ***********************  SECCION DE FILTROS     ***********************************************************/}
             <div className='filters'>
+                <input type='button'
+                    value="Alls Pokemons"
+                    onClick={handleClick}
+                    className="btn-all-poke"
+                />
 
                 {/******************************** FILTRO POR TIPO **********************************/}
-                <input type='button' value="Alls Pokemons" onClick={handleClick} className="btn-all-poke" />
                 <p>Filter by Type:</p>
-                <select name='search' id='search' onChange={(e) => searchType(e.target.value)}>
+                <select
+                    name='search'
+                    id='search'
+                    onChange={(e) => searchType(e.target.value)}
+                >
                     {type ?
                         type.map((e, i) => {
                             return (
@@ -91,14 +105,22 @@ function Home() {
 
                 {/*************************        FILTRO POR CREACION  **********************************/}
                 <p>Filter by:</p>
-                <select name='creation' id='creation' onChange={(e) => searcrCreation(e.target.value)}>
+                <select
+                    name='creation'
+                    id='creation'
+                    onChange={(e) => searcrCreation(e.target.value)}
+                >
                     <option value='pokeApi'>Existing</option>
                     <option value='pokeDb'>Created</option>
                 </select>
 
                 {/*************************        ORDENAMIENTO         **********************************/}
                 <p>Order by:</p>
-                <select name='ordering' id='ordering' onChange={(e) => ordering(e.target.value)}>
+                <select
+                    name='ordering'
+                    id='ordering'
+                    onChange={(e) => ordering(e.target.value)}
+                >
                     <option value='desc'>A - Z</option>
                     <option value='asc'>Z - A</option>
                     <option value='major'>Major attack</option>
@@ -107,14 +129,35 @@ function Home() {
 
                 {/************************          SEARCH              **********************************/}
                 <div className="form__group field">
-                    <input required="" placeholder="Search" className="form__field" value={name} type="input" onChange={(e) => handleInputName(e.target.value)} />
-                    <label className="form__label" htmlFor="name">Name Pokemon</label>
+                    <input
+                        required=""
+                        placeholder="Search"
+                        className="form__field"
+                        value={name}
+                        type="input"
+                        onChange={(e) => handleInputName(e.target.value)}
+                        onKeyDown={handleKeyDown}
+                    />
+                    <label
+                        className="form__label"
+                        htmlFor="name">Name Pokemon
+                    </label>
                 </div>
-                <input type='button' value="Search" name='submit' id='submit' onClick={handleSubmit} className='btn-all-poke' />
+                <input
+                    type='button'
+                    value="Search"
+                    name='submit'
+                    id='submit'
+                    onClick={handleSubmit}
+                    className='btn-all-poke'
+                />
             </div>
 
             {/* //! ***********************  COMPONENTE DE PAGINADO     ********************************************************** */}
-            <Paginate pokemonsPerPage={pokemonsPerPage} allPokemons={allPokemons.length} paginate={paginate} />
+            <Paginate
+                pokemonsPerPage={pokemonsPerPage}
+                allPokemons={allPokemons.length}
+                paginate={paginate} />
 
             {/* //! ***********************  SECCION DE CARDS           ***********************************************************/}
             <div className='cartas'>
