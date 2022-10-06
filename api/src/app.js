57bf436c61801c3c2ad5537d86a28b640d387fcf
Edinson <edinsonmadrid@gmail.com>
2022-10-06 00:07:21 -0300

    cors

--- api/src/app.js
+++ api/src/app.js
@@ -16,7 +16,8 @@ const whiteList = [
   'http://localhost:3001', 
   'https://pokemon-go.up.railway.app/',
   'https://pokemon-go.up.railway.app/pokemons',
-  'https://pokemon-go.up.railway.app/types'
+  'https://pokemon-go.up.railway.app/types',
+  'https://pokemon-edinsondevs.vercel.app'
 ]
   
   server.use(cors({origin:whiteList}));
@@ -26,7 +27,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://pokemon-go.up.railway.app/pokemons'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
